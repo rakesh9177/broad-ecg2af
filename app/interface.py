315e0ed5ec493757d0ad23ed9623e35a469b070a
Rakesh Rathod <112565618+rakesh9177@users.noreply.github.com>
2024-10-23 19:41:55 -0400

    Update interface.py adding docstrings

--- app/interface.py
+++ app/interface.py
@@ -4,13 +4,31 @@ from app.model_handler import ECGModel
 from app.ecg_processor import ECGProcessor
 from app.visualizer import Visualizer
 import logging
+import os
 
 # Initialize logger for the ECGGradioApp
 logger = logging.getLogger(__name__)
 
 
 class ECGGradioApp:
+    """
+    A class representing the Gradio app for ECG predictions.
+
+    Attributes:
+        model (ECGModel): The model used for making ECG predictions.
+        processor (ECGProcessor): The processor used for preprocessing the ECG data.
+        visualizer (Visualizer): The visualizer used for displaying prediction results.
+    """
+
     def __init__(self, ecg_model, ecg_processor, visualizer):
+        """
+        Initialize the ECGGradioApp with model, processor, and visualizer.
+
+        Args:
+            ecg_model (ECGModel): The ECG model for making predictions.
+            ecg_processor (ECGProcessor): The processor for converting ECG data into tensors.
+            visualizer (Visualizer): The visualizer for generating charts from predictions.
+        """
         logger.info("Initializing ECGGradioApp")
         self.model = ecg_model
         self.processor = ecg_processor
@@ -18,7 +36,27 @@ class ECGGradioApp:
         logger.info("ECGGradioApp initialized successfully")
 
     def predict_ecg(self, file):
+        """
+        Process the uploaded ECG file, make predictions, and return the results.
+
+        Args:
+            file (UploadedFile): The ECG file uploaded by the user.
+
+        Returns:
+            tuple: A tuple containing the predictions, including survival curve, sex classification, age prediction, and atrial fibrillation classification.
+
+        Raises:
+            gr.Error: If the file format is not HD5 or an error occurs during the prediction process.
+        """
         logger.info("Received ECG file for prediction: %s", file)
+
+        valid_extensions = ['.hd5']
+        file_extension = os.path.splitext(file.name)[-1].lower()
+
+        if file_extension not in valid_extensions:
+            logger.error("Invalid file format: %s", file_extension)
+            raise gr.Error("Invalid file format. Please upload a file in HD5 format.")
+
         try:
             ecg_tensor = self.processor.ecg_as_tensor(file)
             logger.debug("ECG tensor shape: %s", ecg_tensor.shape)
@@ -27,9 +65,18 @@ class ECGGradioApp:
             return self._generate_outputs(predictions)
         except Exception as e:
             logger.error("Error during prediction: %s", e)
-            return None  # Handle this according to your app's needs
+            raise gr.Error("An error occurred while processing the ECG file. Check your file type and make sure it is in hd5 format")
 
     def _generate_outputs(self, predictions):
+        """
+        Generate outputs based on the model predictions.
+
+        Args:
+            predictions (list): The list of predictions made by the model.
+
+        Returns:
+            tuple: A tuple containing the generated outputs for survival curve, predicted sex, predicted age, and atrial fibrillation classification.
+        """
         logger.info("Generating outputs from predictions")
         outputs = []
         for name, pred in zip(self.model.model_output_names, predictions):
@@ -44,19 +91,25 @@ class ECGGradioApp:
             else:
                 outputs.append(pred)
 
-        output_1 = outputs[0]
+        output_1 = round(outputs[0], 3)
         output_2 = self.visualizer.plot_probability_bar_chart(
             [outputs[1][0][0], outputs[1][0][1]], labels=["Male", "Female"]
         )
-        output_3 = outputs[2][0][0]
+        output_3 = round(outputs[2][0][0], 3)
         output_4 = self.visualizer.plot_probability_bar_chart(
-            [outputs[3][0][0], outputs[3][0][1]]
+            [outputs[3][0][0], outputs[3][0][1]], labels=["Yes", "No"]
         )
 
         logger.info("Outputs generated successfully")
         return output_1, output_2, output_3, output_4
 
     def launch(self):
+        """
+        Launch the Gradio app interface for ECG file upload and prediction.
+
+        The interface allows users to upload an ECG file and receive predictions, which are displayed as sliders and plots.
+
+        """
         logger.info("Launching Gradio interface")
         iface = gr.Interface(
             fn=self.predict_ecg,
@@ -67,11 +120,13 @@ class ECGGradioApp:
                     minimum=0,
                     maximum=1,
                     step=0.01,
-                    elem_id="gradient-slider",
-                    elem_classes=["slider-label"],
+                    info="This gives the probability score of survival curve for incident atrial fibrillation. Range(0 to 1)",
                 ),
-                gr.Plot(label="Predicted Sex"),
-                gr.Slider(label="Predicted Age", minimum=0, maximum=100, step=0.1),
+                gr.Plot(label="Individual Predicted Sex"),
+                gr.Slider(
+                    label="Predicted Age", minimum=0, maximum=100, step=0.1,
+                    info="This gives the age prediction. Negative predicted values will be displayed as zero. Range(0 to 100)"
+                ),
                 gr.Plot(label="Classification of atrial fibrillation"),
             ],
             title="ECG2AF Model Predictions",
@@ -79,5 +134,5 @@ class ECGGradioApp:
             theme=gr.themes.Base(),
         )
 
-        iface.launch()
+        iface.launch(server_name="0.0.0.0")
         logger.info("Gradio interface launched successfully")
